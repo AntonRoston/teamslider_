a43d6f6c17328350428bb2d89ecc5df7220ac50d
AndreyKaras <110786724+AndreyKaras@users.noreply.github.com>
2022-10-09 01:53:12 +0300

    js

---
+++ js/karas.js
@@ -0,0 +1,16 @@
+$('.karas-slider').slick( {
+    arows: true,
+    dots: true,
+    slidesToShow: 3,
+    dotsClass: "karas-dots",
+    centerMode: true,
+    centerPadding: '0px',
+    autoplay: true,
+    autoplaySpeed: 2500,
+    cssEase: 'linear',
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: false,
+});
+
